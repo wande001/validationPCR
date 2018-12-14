6484b969367e3097b8428cd2a72c1d0e3ff0ae6a
Niko Wanders <n.wanders@uu.nl>
2018-12-14 16:27:16 -0500

    Correct input problem

--- plotTimeserie.py
+++ plotTimeserie.py
@@ -27,6 +27,7 @@ def readConfigFile(configFileName):
   return config
 
 def plotCDF(forecast, validation, title):
+  ax1 = plt.figure(figsize=(7,5))
   vals, x1, x2, x3 = cumfreq(forecast['modelled'], len(forecast['modelled']))
   ax1 = plt.plot(np.linspace(np.min(forecast['modelled']), np.max(forecast['modelled']), len(forecast['modelled'])), vals/len(forecast['modelled']), "r", label=str(config.get('Main options', 'RunName')))
   vals, x1, x2, x3 = cumfreq(validation['modelled'], len(validation['modelled']))
@@ -42,6 +43,7 @@ def plotCDF(forecast, validation, title):
   plt.clf()
 
 def plotScatter(forecast, title):
+  ax1 = plt.figure(figsize=(7,5))
   ax1 = plt.plot(forecast['modelled'], forecast['observations'], "bo", markersize=8)
   ax1 = plt.title(title)
   ax1 = plt.xlabel("Modelled")
@@ -51,14 +53,16 @@ def plotScatter(forecast, title):
   plt.clf()
 
 def plotTimeLines(forecast, reference, title):
+  ax1 = plt.figure(figsize=(12,5))
   ax1 = plt.plot(forecast['times'], forecast['modelled'], "r", markersize=8, label=str(config.get('Main options', 'RunName')))
   ax1 = plt.plot(reference['times'], reference['modelled'], "b", markersize=8, label=str(config.get('Reference options', 'RunName')))
-  ax1 = plt.plot(forecast['times'], forecast['observations'], "black", markersize=8, label="Observations")
+  ax1 = plt.plot(forecast['times'], forecast['observations'], "black", markersize=8, label="Observations", lw=2)
+  print forecast['times']
   ax1 = plt.legend(prop={'size': 10}, loc=2)
   ax1 = plt.title(title)
-  ax1 = plt.xlabel("Dates")
+  ax1 = plt.xlabel("")
   ax1 = plt.ylabel("Discharge (m3/s)")
-  ax1 = plt.gcf().set_tight_layout(True)
+  #ax1 = plt.gcf().set_tight_layout(True)
   pdf.savefig()
   plt.clf()
 
@@ -75,14 +79,13 @@ includeRef = getOptions(config, "general")
 output, output2, fullOutput, fullOutput2, waterBalOutput, waterBalOutput2 = pickle.load(open('validationResultsPool_%s_%s.obj' %(run1, run2), 'rb') )
 
 IDs = [i for i,x in enumerate(fullOutput["ID"]) if x == riverID[0]]
-print output[IDs[0]]
 
 pdf = PdfPages('riverResults_%s_%s.pdf' %(run1, run2))
 for ID in IDs:
   
   plotTimeLines(fullOutput["data"][ID], fullOutput2["data"][ID], "River")
-  plotScatter(fullOutput["data"][ID], "Simulations %s, R= %.02d, AC = %.02d, KGE = %.02d" %(str(config.get('Main options', 'RunName')), output[0][ID,3], output[0][ID,4], output[0][ID,5]))
-  plotScatter(fullOutput2["data"][ID], str(config.get('Reference options', 'RunName')))
+  plotScatter(fullOutput["data"][ID], "Simulations %s, R= %.2f, AC = %.2f, KGE = %.2f" %(str(config.get('Main options', 'RunName')), output[IDs[0],3], output[IDs[0],4], output[IDs[0],5]))
+  plotScatter(fullOutput2["data"][ID], "Simulations %s, R= %.2f, AC = %.2f, KGE = %.2f" %(str(config.get('Reference options', 'RunName')), output2[IDs[0],3], output2[IDs[0],4], output2[IDs[0],5]))
   plotCDF(fullOutput["data"][ID], fullOutput2["data"][ID], "River")
 
 pdf.close()
